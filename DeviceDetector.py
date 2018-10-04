140a157c268713f2cf2d3448851d5171e64e204a
jhauserw3241 <joy.hauser@hotmail.com>
2018-10-04 13:41:55 -0500

    Fix a bug with the wrong exit command

--- DeviceDetector.py
+++ DeviceDetector.py
@@ -7,7 +7,7 @@ from utils.WlanPcapFileParser import WlanPcapFileParser
 if len(sys.argv) != 3:
 	print('ERROR: Incorrect number of arguments provided')
 	print('python3 DeviceDetector <pcap_input_directory> <json_output_directory>')
-	error(-1)
+	exit(-1)
 
 parser = WlanPcapFileParser()
 print(parser.getJson(sys.argv[1]))
