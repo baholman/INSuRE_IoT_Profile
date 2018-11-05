0ea6c90d858844b68f80f4e63dde4b1dde8aa144
Blake Holman <baholman@ksu.edu>
2018-11-04 21:58:17 -0600

    edits array size

--- utils/KNN.py
+++ utils/KNN.py
@@ -3,7 +3,7 @@ import json
 from pprint import pprint
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, confusion_matrix 
+from sklearn.metrics import classification_report, confusion_matrix
 
 """
 K-Nearest Neighbor
@@ -64,22 +64,30 @@ class KNN():
                 device_data = json.load(device_file)
             attributes_training = [[0]*len(attributes) for i in range(len(device_data['packets']))]
             for packet in range(len(device_data['packets'])):
+                index = 0
                 for attribute in attributes:
                     if attribute in device_data['packets'][packet]['header']:
-                        attributes_training[packet].append(device_data['packets'][packet]['header'][attribute])
+                        #attributes_training[packet].append(device_data['packets'][packet]['header'][attribute])
+                        attributes_training[packet][index % 2] =  device_data['packets'][packet]['header'][attribute]
                     else:
-                        attributes_training[packet].append(0)
-            print(attributes_training)
+                        #attributes_training[packet].append(0)
+                        attributes_training[packet][index % 2] = 0
+                    index += 1
+    
         for file_name in os.listdir(json_eval_path):
             with open(os.path.join(json_eval_path, file_name)) as device_file:
                 eval_data = json.load(device_file)
             attributes_eval = [[0]*len(attributes) for i in range(len(eval_data['packets']))]
             for packet in range(len(eval_data['packets'])):
+                index = 0
                 for attribute in attributes:
                     if attribute in eval_data['packets'][packet]['header']:
-                        attributes_eval[packet].append(eval_data['packets'][packet]['header'][attribute])
+                        #attributes_eval[packet].append(eval_data['packets'][packet]['header'][attribute])
+                        attributes_eval[packet][index % 2] = eval_data['packets'][packet]['header'][attribute]
                     else:
-                        attributes_eval[packet].append(0)
+                        #attributes_eval[packet].append(0)
+                        attributes_eval[packet][index % 2] = 0
+                    index+=1
 
         self.__scaleFeatures(attributes_training, attributes_eval)
         self.__trainAndPredict(attributes, attributes_training, attributes_eval)
@@ -94,7 +102,7 @@ class KNN():
 
 
     def __trainAndPredict(self, attributes, attributes_training, attributes_eval):
-        n_neighbors_count = len(attributes)
+        n_neighbors_count = 6
         classifier = KNeighborsClassifier(n_neighbors=n_neighbors_count)  
         classifier.fit(attributes_training, attributes)
         eval_pred = classifier.predict(attributes_eval)
